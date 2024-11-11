b247204121c91492dedd623aca94ebc4b01f833f
rajathmCMU <rajathkotyal@gmail.com>
2024-11-11 01:50:40 -0800

    LRU redis update to count 5

---
+++ audio/audio.go
@@ -0,0 +1,77 @@
+import(
+	"github.com/PuerkitoBio/goquery"
+	readability "github.com/go-shiori/go-readability"
+	"github.com/texttheater/golang-levenshtein/levenshtein"
+	"encoding/base64"
+    "google.golang.org/api/option"
+    texttospeech "cloud.google.com/go/texttospeech/apiv1"
+    texttospeechpb "google.golang.org/genproto/googleapis/cloud/texttospeech/v1"
+    "github.com/gorilla/websocket"
+    "github.com/redis/go-redis/v9"
+)
+
+const (
+	googleSearchURL = "https://www.googleapis.com/customsearch/v1"
+)
+
+// Audio conversion
+func HandleUserInteraction(sessionID string, userInput string, wsConn *websocket.Conn) error {
+	var immediateResponse string
+	var err error
+
+	immediateResponse, err = GenerateResponse(userInput)
+	if err != nil {
+			log.Println("Error generating response:", err)
+	}
+
+	audioData, err := ConvertTextToSpeech(immediateResponse)
+	if err != nil {
+			log.Println("Error converting text to speech:", err)
+			return err
+	}
+
+	err = StreamAudio(wsConn, audioData)
+	if err != nil {
+			log.Println("Error streaming audio:", err)
+			return err
+	}
+	return nil
+}
+
+func ConvertTextToSpeech(text string) ([]byte, error) {
+	ctx := context.Background()
+	client, err := texttospeech.NewClient(ctx, option.WithAPIKey(GoogleCloudAPIKey))
+	if err != nil {
+			return nil, fmt.Errorf("failed to create TTS client: %w", err)
+	}
+	defer client.Close()
+
+	req := &texttospeechpb.SynthesizeSpeechRequest{
+			Input: &texttospeechpb.SynthesisInput{
+					InputSource: &texttospeechpb.SynthesisInput_Text{Text: text},
+			},
+			Voice: &texttospeechpb.VoiceSelectionParams{
+					LanguageCode: "en-US",
+					SsmlGender:   texttospeechpb.SsmlVoiceGender_NEUTRAL,
+			},
+			AudioConfig: &texttospeechpb.AudioConfig{
+					AudioEncoding: texttospeechpb.AudioEncoding_OGG_OPUS,
+			},
+	}
+
+	resp, err := client.SynthesizeSpeech(ctx, req)
+	if err != nil {
+			return nil, fmt.Errorf("failed to synthesize speech: %w", err)
+	}
+
+	return resp.AudioContent, nil
+}
+
+func StreamAudio(conn *websocket.Conn, audioData []byte) error {
+	// binary message
+	err := conn.WriteMessage(websocket.BinaryMessage, audioData)
+	if err != nil {
+			return fmt.Errorf("failed to send audio data: %w", err)
+	}
+	return nil
+}
